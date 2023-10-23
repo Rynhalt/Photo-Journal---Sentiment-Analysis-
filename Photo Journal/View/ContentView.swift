eb223a59a82303f0e96f5423ae7d2c135bbd0ea5
Marcus Chang <30785471+Rynhalt@users.noreply.github.com>
2023-10-22 22:33:34 -0400

    Main Functions managin user Data

--- Photo Journal/View/ContentView.swift
+++ Photo Journal/View/ContentView.swift
@@ -9,11 +9,53 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
+    @State var showImagePicker: Bool = false
+    
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        NavigationView {
+            VStack{
+                if true {
+                    Text("Add a note")
+                        .italic()
+                        .foregroundColor(.gray)
+                } else {
+                    HomeView()
+                }
+            }
+            .navigationTitle("Self-Analysis")
+            .sheet(isPresented: $showImagePicker) {
+                ImagePicker(sourceType: .photoLibrary) {
+                    image in
+                    // Add notes and images to array
+                    print("\(image.scale)")
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showImagePicker.toggle()
+                    } label: {
+                        Label("Image", systemImage: "photo.on.rectangle.angled")
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        withAnimation {
+                            //Reset user data: journals
+                        }
+                    } label: {
+                        Label("Trash", systemImage: "trash")
+                    }
+                    .tint(.red)
+                }
+            }
+        }
     }
 }
 
+    
 #Preview {
     ContentView()
 }
